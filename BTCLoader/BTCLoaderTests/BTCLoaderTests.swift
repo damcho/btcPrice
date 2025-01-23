eabcb455977b49ee8a681bc19806b9beaa43f7b0
Damian Modernell <dmodernell@cohesionib.com>
2025-01-23 17:04:36 -0300

    test BTCLoader throws decoding error on mapping failure

--- BTCLoader/BTCLoaderTests/BTCLoaderTests.swift
+++ BTCLoader/BTCLoaderTests/BTCLoaderTests.swift
@@ -8,4 +8,30 @@
 @testable import BTCLoader
 import XCTest
 
-final class BTCLoaderTests: XCTestCase {}
+struct BTCPrice {}
+
+enum RemoteBTCPriceLoaderError: Error {
+    case decding
+}
+
+struct RemoteBTCPriceLoader {
+    func loadBTCPrice() async throws(RemoteBTCPriceLoaderError) -> BTCPrice {
+        throw .decding
+    }
+}
+
+final class BTCLoaderTests: XCTestCase {
+    func test_throws_error_on_mapping_failure() async {
+        let sut = makeSUT()
+
+        await XCTAssertThrowsError(
+            try await sut.loadBTCPrice()
+        )
+    }
+}
+
+extension BTCLoaderTests {
+    func makeSUT() -> RemoteBTCPriceLoader {
+        RemoteBTCPriceLoader()
+    }
+}
