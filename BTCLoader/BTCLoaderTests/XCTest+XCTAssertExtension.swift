ad7a9bfeb5da977195a590afda4e87817a40a5ec
Damian Modernell <dmodernell@cohesionib.com>
2025-01-23 17:03:55 -0300

    adding XCTest extension to allow error assertion in async methods

---
+++ BTCLoader/BTCLoaderTests/XCTest+XCTAssertExtension.swift
@@ -0,0 +1,24 @@
+//
+//  XCTest+XCTAssertExtension.swift
+//  BTCLoaderTests
+//
+//  Created by Damian Modernell on 23/1/25.
+//
+import XCTest
+
+extension XCTest {
+    func XCTAssertThrowsError(
+        _ expression: @autoclosure () async throws -> some Sendable,
+        _ message: @autoclosure () -> String = "This call should throw an error.",
+        file: StaticString = #filePath,
+        line: UInt = #line,
+        _ errorHandler: (_ error: Error) -> Void = { _ in }
+    ) async {
+        do {
+            _ = try await expression()
+            XCTFail(message(), file: file, line: line)
+        } catch {
+            errorHandler(error)
+        }
+    }
+}
