5db0286196b254851576220fb7c1d14230271ddd
Damian Modernell <dmodernell@cohesionib.com>
2025-01-23 15:44:16 -0300

    Created new macos framework project to host the BTC-loader-framework

---
+++ BTCLoader/BTCLoader/BTCLoader.h
@@ -0,0 +1,18 @@
+//
+//  BTCLoader.h
+//  BTCLoader
+//
+//  Created by Damian Modernell on 23/1/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BTCLoader.
+FOUNDATION_EXPORT double BTCLoaderVersionNumber;
+
+//! Project version string for BTCLoader.
+FOUNDATION_EXPORT const unsigned char BTCLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BTCLoader/PublicHeader.h>
+
+
---
+++ BTCLoader/BTCLoaderTests/BTCLoaderTests.swift
@@ -0,0 +1,14 @@
+//
+//  BTCLoaderTests.swift
+//  BTCLoaderTests
+//
+//  Created by Damian Modernell on 23/1/25.
+//
+
+import XCTest
+@testable import BTCLoader
+
+final class BTCLoaderTests: XCTestCase {
+
+
+}
