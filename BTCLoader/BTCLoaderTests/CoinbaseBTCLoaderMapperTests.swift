77cfab440031e9d529b74fc8154a9a0f9fed6208
Damian Modernell <dmodernell@cohesionib.com>
2025-01-24 18:39:23 -0300

    Implemented CoinbaseBTCLoaderMapper

--- BTCLoader/BTCLoaderTests/CoinbaseBTCLoaderMapperTests.swift
+++ BTCLoader/BTCLoaderTests/CoinbaseBTCLoaderMapperTests.swift
@@ -8,25 +8,65 @@
 import XCTest
 @testable import BTCLoader
 
+enum CoinbaseBTCLoaderMapper {
+    struct CoinbaseBTCPrice: Decodable {
+        let display: PriceData
+        
+        enum CodingKeys: String, CodingKey {
+            case display = "DISPLAY"
+        }
+
+        func toBTCPrice() throws -> BTCPrice {
+            let trimmedDisplay = display.price.replacingOccurrences(of: " ", with: "")
+            let formatter = NumberFormatter()
+            formatter.locale = Locale(identifier: "en_US")
+            formatter.numberStyle = .currency
+            guard let aPrice = formatter.number(from: trimmedDisplay) else {
+                throw RemoteBTCPriceLoaderError.decoding
+            }
+            return BTCPrice(amount: Double(truncating: aPrice), currency: .USD)
+        }
+    }
+    
+    struct PriceData: Decodable {
+        let price: String
+        
+        enum CodingKeys: String, CodingKey {
+            case price = "PRICE"
+        }
+    }
+
+    static func map(_ response: HTTPURLResponse, _ data: Data) throws -> BTCPrice {
+        guard response.statusCode == 200 else {
+            throw RemoteBTCPriceLoaderError.connectivity
+        }
+        let coinbaseBTCPrice = try JSONDecoder().decode(
+            CoinbaseBTCPrice.self,
+            from: data
+        )
+        return try coinbaseBTCPrice.toBTCPrice()
+    }
+}
+
 final class CoinbaseBTCLoaderMapperTests: XCTestCase {
 
     func test_throws_on_non_200_http_response() throws {
         XCTAssertThrowsError(
-            try BinanceBTCLoaderMapper.map(non200HTTPResponse, anyData)
+            try CoinbaseBTCLoaderMapper.map(non200HTTPResponse, anyData)
         )
     }
     
     func test_throws_on_200_response_and_empty_data() throws {
         let emptyData = Data()
         XCTAssertThrowsError(
-            try BinanceBTCLoaderMapper.map(validHTTPResponse, emptyData)
+            try CoinbaseBTCLoaderMapper.map(validHTTPResponse, emptyData)
         )
     }
     
     func test_maps_btc_price_successfully() throws {
         let expectedBTCPrice = anyBTCPrice
         
-        let decodedBTCprice = try BinanceBTCLoaderMapper.map(
+        let decodedBTCprice = try CoinbaseBTCLoaderMapper.map(
             validHTTPResponse, expectedBTCPrice.encoded
         )
         
@@ -37,8 +77,8 @@ final class CoinbaseBTCLoaderMapperTests: XCTestCase {
 private extension CoinbaseBTCLoaderMapperTests {
     var anyBTCPrice: (decoded: BTCPrice, encoded: Data) {
         (
-            BTCPrice(amount: 104593.190, currency: .USD),
-            #"{"symbol":"BTCUSDT","price":"104593.19000000"}"#.data(using: .utf8)!
+            BTCPrice(amount: 103457.5, currency: .USD),
+            #"{"RAW":{"MARKET":"CUSTOMAGG","FROMSYMBOL":"BTC","TOSYMBOL":"USD","FLAGS":0,"PRICE":103457.51,"LASTUPDATE":1737685911,"LASTVOLUME":0.00000183,"LASTVOLUMETO":0.1893272433,"LASTTRADEID":"766587058","VOLUME24HOUR":24311.60952878,"VOLUME24HOURTO":2535660127.21734,"OPEN24HOUR":102574.45,"HIGH24HOUR":106870.87,"LOW24HOUR":101200.01,"LASTMARKET":"Coinbase","TOPTIERVOLUME24HOUR":24311.60952878,"TOPTIERVOLUME24HOURTO":2535660127.21734,"CHANGE24HOUR":883.059999999998,"CHANGEPCT24HOUR":0.860896646289595,"CHANGEDAY":0,"CHANGEPCTDAY":0,"CHANGEHOUR":0,"CHANGEPCTHOUR":0},"DISPLAY":{"FROMSYMBOL":"Ƀ","TOSYMBOL":"$","MARKET":"CUSTOMAGG","PRICE":"$ 103,457.5","LASTUPDATE":"Just now","LASTVOLUME":"Ƀ 0.00000183","LASTVOLUMETO":"$ 0.1893","LASTTRADEID":"766587058","VOLUME24HOUR":"Ƀ 24,311.6","VOLUME24HOURTO":"$ 2,535,660,127.2","OPEN24HOUR":"$ 102,574.5","HIGH24HOUR":"$ 106,870.9","LOW24HOUR":"$ 101,200.0","LASTMARKET":"Coinbase","TOPTIERVOLUME24HOUR":"Ƀ 24,311.6","TOPTIERVOLUME24HOURTO":"$ 2,535,660,127.2","CHANGE24HOUR":"$ 883.06","CHANGEPCT24HOUR":"0.86","CHANGEDAY":"$ 0","CHANGEPCTDAY":"0","CHANGEHOUR":"$ 0","CHANGEPCTHOUR":"0"}}"#.data(using: .utf8)!
         )
     }
 }
