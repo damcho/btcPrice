41a1e4d0daa5efeb296a68c0eb997e3675bf6462
Damian Modernell <dmodernell@cohesionib.com>
2025-01-24 17:02:19 -0300

    testmaps binance btc price susccessfully

--- BTCLoader/BTCLoaderTests/BinanceBTCLoaderMapperTests.swift
+++ BTCLoader/BTCLoaderTests/BinanceBTCLoaderMapperTests.swift
@@ -9,8 +9,26 @@ import XCTest
 @testable import BTCLoader
 
 enum BinanceBTCLoaderMapper {
+    struct BinanceBTCPrice: Decodable {
+        let price: String
+        
+        func toBTCPrice() throws -> BTCPrice {
+            guard let aPrice = Double(price) else {
+                throw RemoteBTCPriceLoaderError.decoding
+            }
+            return BTCPrice(amount: aPrice , currency: .USD)
+        }
+    }
+    
     static func map(_ response: HTTPURLResponse, _ data: Data) throws -> BTCPrice {
-        throw RemoteBTCPriceLoaderError.connectivity
+        guard response.statusCode == 200 else {
+            throw RemoteBTCPriceLoaderError.connectivity
+        }
+        let binanceBTCPrice = try JSONDecoder().decode(
+            BinanceBTCPrice.self,
+            from: data
+        )
+        return try binanceBTCPrice.toBTCPrice()
     }
 }
 
@@ -26,6 +44,25 @@ final class BinanceBTCLoaderMapperTests: XCTestCase {
         let emptyData = Data()
         XCTAssertThrowsError(
             try BinanceBTCLoaderMapper.map(validHTTPResponse, emptyData)
+        )
+    }
+    
+    func test_maps_btc_price_successfully() throws {
+        let expectedBTCPrice = anyBTCPrice
+        
+        let decodedBTCprice = try BinanceBTCLoaderMapper.map(
+            validHTTPResponse, expectedBTCPrice.encoded
+        )
+        
+        XCTAssertEqual(decodedBTCprice, expectedBTCPrice.decoded)
+    }
+}
+
+private extension BinanceBTCLoaderMapperTests {
+    var anyBTCPrice: (decoded: BTCPrice, encoded: Data) {
+        (
+            BTCPrice(amount: 104593.190, currency: .USD),
+            #"{"symbol":"BTCUSDT","price":"104593.19000000"}"#.data(using: .utf8)!
         )
     }
 }
