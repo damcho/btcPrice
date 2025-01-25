0a81fd47ee70d2adc67f09f1061330a1c941c38d
Damian Modernell <dmodernell@cohesionib.com>
2025-01-25 18:24:09 -0300

    Creating iOS app project and adding it to the BTCUtility workspace

---
+++ BTCPriceLoaderApp/BTCPriceLoaderApp/BTCPriceLoaderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTCPriceLoaderAppApp.swift
+//  BTCPriceLoaderApp
+//
+//  Created by Damian Modernell on 25/1/25.
+//
+
+import SwiftUI
+
+@main
+struct BTCPriceLoaderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BTCPriceLoaderApp/BTCPriceLoaderApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BTCPriceLoaderApp
+//
+//  Created by Damian Modernell on 25/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
