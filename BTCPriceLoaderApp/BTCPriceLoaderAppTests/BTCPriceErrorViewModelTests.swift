c8e027d2f04368cd80f2592f883d55487de789f2
Damian Modernell <dmodernell@cohesionib.com>
2025-01-26 15:02:17 -0300

    Improving  test_displays_error_with_no_btc_price_on_no_btc_price_available

--- BTCPriceLoaderApp/BTCPriceLoaderAppTests/BTCPriceErrorViewModelTests.swift
+++ BTCPriceLoaderApp/BTCPriceLoaderAppTests/BTCPriceErrorViewModelTests.swift
@@ -9,13 +9,17 @@ import XCTest
 
 final class BTCPriceErrorViewModel {
     var errorLabel: String = "Failed to load BTC price"
+    
+    func displayBTCLoadError() {}
 }
 
 final class BTCPriceErrorViewModelTests: XCTestCase {
 
-    func test_displays_error_with_no_btc_price_on_initialization() {
+    func test_displays_error_with_no_btc_price_on_no_btc_price_available() {
         let btcErrorViewModel = BTCPriceErrorViewModel()
+        
+        btcErrorViewModel.displayBTCLoadError()
+        
         XCTAssertEqual(btcErrorViewModel.errorLabel, "Failed to load BTC price")
     }
-
 }
