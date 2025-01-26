cd716db6f98164f9cf2de15655125fd85ffb93fd
Damian Modernell <dmodernell@cohesionib.com>
2025-01-26 14:31:02 -0300

    Refactoring initial test by adding ad BTCPriceViewRepresentation struct that holds btc price label and color to display

--- BTCPriceLoaderApp/BTCPriceLoaderAppTests/BTCPriceViewModelTests.swift
+++ BTCPriceLoaderApp/BTCPriceLoaderAppTests/BTCPriceViewModelTests.swift
@@ -9,14 +9,24 @@ import XCTest
 @testable import BTCPriceLoaderApp
 @testable import BTCLoader
 
+struct BTCPriceViewRepresentation {
+    let price: String
+    let color: UIColor
+}
+
 final class BTCPriceViewModel {
-    var btcPrice: BTCPrice?
+    var btcPriceLabel: String = ""
+    var btcPrice: BTCPriceViewRepresentation = .init(price: "", color: .black) {
+        didSet {
+            btcPriceLabel = "BTC/USD: \(btcPrice.price)"
+        }
+    }
 }
 
 final class BTCPriceViewModelTests: XCTestCase {
-    func test_displays_no_btc_on_initial_state() {
+    func test_displays_empty_string_on_initialization() {
         let bTCPriceViewModel = BTCPriceViewModel()
-        XCTAssertNil(bTCPriceViewModel.btcPrice)
+        XCTAssertEqual(bTCPriceViewModel.btcPrice.color, .black)
+        XCTAssertEqual(bTCPriceViewModel.btcPrice.price, "")
     }
-
 }
