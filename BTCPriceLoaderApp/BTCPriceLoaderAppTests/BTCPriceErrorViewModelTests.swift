6c034c6d23c57f2b098f53260cec87b2338e7021
Damian Modernell <dmodernell@cohesionib.com>
2025-01-26 15:35:42 -0300

    test_displays_error_with_btc_price_on_btc_price_available

--- BTCPriceLoaderApp/BTCPriceLoaderAppTests/BTCPriceErrorViewModelTests.swift
+++ BTCPriceLoaderApp/BTCPriceLoaderAppTests/BTCPriceErrorViewModelTests.swift
@@ -6,20 +6,62 @@
 //
 
 import XCTest
+import Foundation
 
 final class BTCPriceErrorViewModel {
-    var errorLabel: String = "Failed to load BTC price"
+    private(set)var errorLabel: String = "Failed to load BTC price"
+    var lastUpdatedBTCPriceDate: Date?
+    private let dateformatter: DateFormatter
     
-    func displayBTCLoadError() {}
+    init(dateFormatter: DateFormatter = DateFormatter()) {
+        self.dateformatter = dateFormatter
+    }
+    
+    func displayBTCLoadError() {
+        if let alastUpdatedBTCPriceDate = lastUpdatedBTCPriceDate {
+            errorLabel = "Failed to update value. Displaying last updated value from \(dateformatter.string(from: alastUpdatedBTCPriceDate))"
+        }
+    }
 }
 
 final class BTCPriceErrorViewModelTests: XCTestCase {
 
     func test_displays_error_with_no_btc_price_on_no_btc_price_available() {
-        let btcErrorViewModel = BTCPriceErrorViewModel()
+        let btcErrorViewModel = makeSUT()
         
         btcErrorViewModel.displayBTCLoadError()
         
         XCTAssertEqual(btcErrorViewModel.errorLabel, "Failed to load BTC price")
+    }
+    
+    func test_displays_error_with_btc_price_on_btc_price_available() {
+        let btcErrorViewModel = makeSUT()
+        btcErrorViewModel.lastUpdatedBTCPriceDate = randomDate.date
+        
+        btcErrorViewModel.displayBTCLoadError()
+        
+        XCTAssertEqual(btcErrorViewModel.errorLabel, "Failed to update value. Displaying last updated value from \(randomDate.stringRepresentation)")
+    }
+}
+
+extension BTCPriceErrorViewModelTests {
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd HH:mm"
+        return formatter
+    }
+    
+    func makeSUT() -> BTCPriceErrorViewModel {
+        BTCPriceErrorViewModel(
+            dateFormatter: dateFormatter
+        )
+    }
+    
+    var randomDate: (date: Date, stringRepresentation: String) {
+        let aDate = dateFormatter.date(from: "2016/10/08 22:31")!
+        return (
+            aDate,
+            dateFormatter.string(from: aDate)
+        )
     }
 }
