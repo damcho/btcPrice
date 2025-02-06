3f9c713829417e9aa0b7832abe9951674bc3106d
Damian Modernell <dmodernell@cohesionib.com>
2025-02-06 16:33:37 -0300

    BybitMapper test_throws_on_non_200_http_response

--- BTCLoader/BTCLoaderTests/BybitBTCLoaderMapperTests.swift
+++ BTCLoader/BTCLoaderTests/BybitBTCLoaderMapperTests.swift
@@ -6,10 +6,24 @@
 //
 
 import XCTest
+@testable import BTCLoader
+
+struct BybitBTCLoaderMapper {
+    static func map(http: (response: HTTPURLResponse, data: Data)) throws -> RemoteBTCPrice {
+        guard http.response.statusCode == 200 else {
+            throw RemoteBTCPriceLoaderError.connectivity
+        }
+        return .init(amount: 10, currency: .USD)
+    }
+}
 
 final class BybitBTCLoaderMapperTests: XCTestCase, BTCMapperSpecs {
     func test_throws_on_non_200_http_response() throws {
-       
+        XCTAssertThrowsError(
+            try BybitBTCLoaderMapper.map(
+                http: (non200HTTPResponse, anyData)
+            )
+        )
     }
     
     func test_throws_on_200_response_and_empty_data() throws {
