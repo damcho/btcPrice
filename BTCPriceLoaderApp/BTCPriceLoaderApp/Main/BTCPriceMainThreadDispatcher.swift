f13c83830e9ab3e783789f2fb769e85d96e0f9c3
Damian Modernell <dmodernell@cohesionib.com>
2025-02-08 20:21:20 -0300

    Creating object BTCPriceMainThreadDispatcher to dispatch all calls to the corresponding viewmodels on the main thread

---
+++ BTCPriceLoaderApp/BTCPriceLoaderApp/Main/BTCPriceMainThreadDispatcher.swift
@@ -0,0 +1,41 @@
+//
+//  BTCPriceMainThreadDispatcher.swift
+//  BTCPriceLoaderApp
+//
+//  Created by Damian Modernell on 8/2/25.
+//
+
+import Foundation
+import BTCUtilityCore
+
+struct BTCPriceMainThreadDispatcher<BTCDisplayable> {
+    let decoratee: BTCDisplayable
+}
+
+// MARK: BTCPriceDisplayable
+
+extension BTCPriceMainThreadDispatcher: BTCPriceDisplayable where BTCDisplayable == BTCPriceDisplayable {
+    func display(_ btcPrice: BTCPrice) {
+        Task { @MainActor in
+            decoratee.display(btcPrice)
+        }
+    }
+}
+
+// MARK: BTCPriceErrorDisplayable
+
+extension BTCPriceMainThreadDispatcher: BTCPriceErrorDisplayable where BTCDisplayable == BTCPriceErrorDisplayable {
+    func displayBTCLoadError(for timestamp: Date?) {
+        Task { @MainActor in
+            decoratee.displayBTCLoadError(for: timestamp)
+        }
+    }
+}
+
+extension BTCPriceMainThreadDispatcher: BTCPriceErrorRemovable where BTCDisplayable == BTCPriceErrorRemovable {
+    func hideBTCLoadError() {
+        Task { @MainActor in
+            decoratee.hideBTCLoadError()
+        }
+    }
+}
